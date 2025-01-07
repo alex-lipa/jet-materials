7d4672554b2b21d90bd36641deb54598b35f8e21
Alexey Mazaev <mazaev.a@spb-ifuture.com>
2025-01-07 13:46:30 +0300

    MY

--- 02-learning-jetpack-compose-fundamentals/projects/starter/app/src/main/java/com/yourcompany/android/jetpackcompose/screens/ButtonsScreen.kt
+++ 02-learning-jetpack-compose-fundamentals/projects/starter/app/src/main/java/com/yourcompany/android/jetpackcompose/screens/ButtonsScreen.kt
@@ -34,12 +34,23 @@
 
 package com.yourcompany.android.jetpackcompose.screens
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.yourcompany.android.jetpackcompose.R
 import com.yourcompany.android.jetpackcompose.router.BackButtonHandler
 import com.yourcompany.android.jetpackcompose.router.JetFundamentalsRouter
 import com.yourcompany.android.jetpackcompose.router.Screen
@@ -62,12 +73,31 @@ fun ExploreButtonsScreen() {
 
 @Composable
 fun MyButton() {
-  //TODO add your code here
+  Button(
+      onClick = {},
+      colors = ButtonDefaults.buttonColors(
+          backgroundColor = colorResource(id = R.color.colorPrimary)),
+      border = BorderStroke(
+          1.dp,
+          color = colorResource(id = R.color.colorPrimaryDark))
+  ) {
+      Text(
+          text = stringResource(id = R.string.button_text),
+          color = Color.White
+      )
+  }
 }
 
 @Composable
 fun MyRadioGroup() {
-  //TODO add your code here
+    val radioButtons = listOf(0, 1, 2)
+    val selectedButton = remember { mutableStateOf(radioButtons.first()) }
+
+    Column {
+        radioButtons.forEach { index ->
+
+        }
+    }
 }
 
 @Composable
